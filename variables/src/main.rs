1d4a6c94bdf85f9be41035725e44a68b204e8284
Mitsu <tech.mitsu.yuki@gmail.com>
2024-08-08 16:23:53 +0900

    Add panic exmaple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,29 @@
+use std::io;
+
 fn main() {
-    let x = 5;
+    let a = [1, 2, 3, 4, 5];
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x is {}", x);
-    }
+    println!("Please enter an array index.");
+           // 配列の何番目の要素にアクセスするか指定してください
 
-    println!("The value of x is {}", x);
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+              // 値の読み込みに失敗しました
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+        // 入力された値は数字ではありません
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        // {}番目の要素の値は{}です
+        index, element
+    );
 }
