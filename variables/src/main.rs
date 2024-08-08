18b42864fb03f24f61fa0722b0a56d23c50fc177
Mitsu <tech.mitsu.yuki@gmail.com>
2024-08-08 16:07:59 +0900

    Add shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let x = 5;
-    println!("The value of x is {}", x);
-    x = 6;
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x is {}", x);
+    }
+
     println!("The value of x is {}", x);
 }
