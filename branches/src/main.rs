c56e622b98b5cfccac8e32c5909b0cbdd3f61b68
Mitsu <tech.mitsu.yuki@gmail.com>
2024-08-08 17:11:04 +0900

    Add if statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 7;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
