a1038b840ad62227eb0ab3635d06719f43b03721
Mitsu <tech.mitsu.yuki@gmail.com>
2024-08-09 16:13:35 +0900

    Mod struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,10 +1,20 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!("The area of the rectangle is {} squeare pixels.", area(width1, height1))
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} squeare pixels.",
+        area(&rect1)
+    )
+}
+
+fn area(rectrangle: &Rectangle) -> u32 {
+    rectrangle.width * rectrangle.height
 }
